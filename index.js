5f940e91a4b893d8fecf82570c1f0f7437570c49
iba83 <sson.joer@icloud.com>
2020-06-01 20:16:37 +0900

    topedit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 $(function(){
-  $(".right").ripples({
+  $(".main").ripples({
     dropRadius: 20,
     perturbance: 0,
     resolution: 240,
   });
+
+  $(".top").hide().fadeIn(1500);
+
+  $("profilebtn").on("click", function(){
+    
+  })
 })
